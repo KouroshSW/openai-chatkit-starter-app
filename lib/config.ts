10cd650b0060bb113a1bac08428aa8364050047f
KouroshSW <kourosh@agerakbt.se>
2025-10-16 18:12:01 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -7,7 +7,7 @@ export const CREATE_SESSION_ENDPOINT = "/api/create-session";
 
 export const STARTER_PROMPTS: StartScreenPrompt[] = [
   {
-    label: "Vad kan du göra",
+    label: "Vad kan du göra?",
     prompt: "What can you do?",
     icon: "circle-question",
   },
